70cbb668822add11be4e7d986c9bdfd25b456424
Paulo Turco <pauloturcko@gmail.com>
2024-12-23 22:13:08 -0300

    Project setting up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ToggleMenu from './Components/Atoms/ToggleMenu';
+
+const App = () => {
+  return (
+    <div>
+      <ToggleMenu />
+    </div>
+  )
+};
+
+export default App;
